d0c5347a60d141b5187a185768a9bd007bbf5854
GouchinHub <69035493+GouchinHub@users.noreply.github.com>
2022-03-07 18:53:43 +0200

    ChatApp v2

--- client.py
+++ client.py
@@ -1,33 +1,56 @@
 #==========================================
 # Title:  ChatApp client
 # Author: Aatu Laitinen
-# Date:   1 March 2021
+# Date:   6 March 2022
 #==========================================
 
 import socket
 import threading
 import time
 
-room = input("Choose room to join('Room1','Room2','Room3'): ")
-nickname = input("Choose a nickname: ")
+nickname = input("Enter your nickname: ")
+
 #client connets to the server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 1337))
+client.connect(('127.0.0.1', 1234))
 
-def recieve():
+def establish_connection():
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
             if(message == 'ROOM'):
+                room = input("\nType the name of the room you want to join or create: ")
                 client.send(room.encode('utf-8'))
-            elif(message == 'NICK'):
+            elif(message == 'NOROOM'):
+                print(f'Room [{room}] doesnt exist')
+                is_room_created = input(f'Do you want to create room: [{room}]? (yes / no): ')
+                if is_room_created.lower() == 'yes':
+                    client.send("YES".encode('utf-8'))
+                else:
+                    client.send("NO".encode('utf-8'))
+                    close_client()
+                    break
+            elif(message == 'NICKNAME'):
                 client.send(nickname.encode('utf-8'))
+            elif(message == 'CONNECTED'):
+                setup_complete()
+                break
+            elif(message == 'DISCONNECTED'):
+                close_client()
+                break
             else:
-                print(message)
+                print(f'{message}')
         except:
             print("Error occured!")
-            time.sleep(1)
-            client.close()
+            close_client()
+            break
+
+def recieve():
+    while True:
+        try:
+            print(client.recv(1024).decode('utf-8')) 
+        except:
+            close_client()
             break
 
 def write():
@@ -39,12 +62,19 @@ def write():
         #/exit functionality to leave the server
         elif message[len(nickname)+2:] == '/exit':
             client.send('EXIT'.encode('utf-8'))
+            client.close()
+            break
         else:
             client.send(message.encode('utf-8'))
 
-#threading to allow multiple clients
-recieve_thread = threading.Thread(target=recieve)
-recieve_thread.start()
-
-write_thread = threading.Thread(target=write)
-write_thread.start()
+def close_client():
+    print("Closing client...")
+    time.sleep(1)
+    client.close()
+            
+def setup_complete():
+    #threading to allow multiple clients
+    threading.Thread(target=recieve).start()
+    threading.Thread(target=write).start()
+    
+threading.Thread(target=establish_connection).start()
--- server.py
+++ server.py
@@ -1,142 +1,183 @@
 #==========================================
 # Title:  ChatApp server
 # Author: Aatu Laitinen
-# Date:   1 March 2021
+# Date:   6 March 2022
 #==========================================
 
 import threading
 import socket
 import time
 
+NON_EXISTENT = -1
+BUFFER_SIZE = 1024
+
 host = '127.0.0.1' #localhost
-port = 1337
+port = 1234
 
 #creates the server using internet sockets
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
 server.listen()
 
-#Amount of available rooms on the server can be changed here
-#changes are also needed to handle_room function
-room1 = []
-room2 = []
-room3 = []
-rooms = [room1,room2,room3]
 
-allclients = []
+class Room:
+    def __init__(self, name):
+        self.name = name
+        self.clients = []
+        
+    def add_client(self, client):
+        self.clients.append(client)
+    
+    def remove_client(self, client):
+        self.clients.remove(client)
+
+
+#Storage for room and client information
+rooms_list = [Room("default_room")]
+all_clients = []
 nicknames = []
 
-#broadcasts message to all the clients in the same room
-def broadcast(roomindex,message):
-    for client in rooms[roomindex]:
-        client.send(message)
 
-#sends private message to the target client if connected to server
-def private_message(message_to,message_from, message_to_client):
-    try:
-        if message_to in nicknames:
-            targetindex = nicknames.index(message_to)
-            target_client = allclients[targetindex]
-            target_client.send(f'FROM {message_from}: {message_to_client}'.encode('utf-8'))
+def search_client_connections():
+    while True:
+        client, address = server.accept()
+        print(f"Client trying to connect from address: {str(address)}")
+        threading.Thread(target=process_client_connections, args=(client, address)).start()
+        
+        
+#Recieves and processes the client connections
+def process_client_connections(client, address):
+    #retrieve client nickname
+    client.send('NICKNAME'.encode('utf-8'))
+    nickname = client.recv(BUFFER_SIZE).decode('utf-8')
+    
+    #check nickname availability and add client to corresponding room
+    if nickname in nicknames:
+        client.send(f'Nickname [{nickname}] already taken'.encode('utf-8'))
+        client.send("DISCONNECTED".encode('utf-8'))
+        client.close()
+        print(f"connection {str(address)} terminated")
+        return
+    
+    #provide client with list of available rooms
+    client.send('Available rooms: '.encode('utf-8'))
+    for room in rooms_list:
+        time.sleep(0.01)
+        client.send((f'\t{room.name}').encode('utf-8'))
+    
+    time.sleep(0.01)
+    
+    #get and handle client chosen room
+    client.send('ROOM'.encode('utf-8'))
+    room_name = client.recv(BUFFER_SIZE).decode('utf-8')
+    roomindex = get_room_index(room_name)
+    
+    if(roomindex == NON_EXISTENT):
+        client.send('NOROOM'.encode('utf-8'))
+        create = client.recv(BUFFER_SIZE).decode('utf-8')
+        if (create == 'YES'):
+            room = Room(room_name)
+            rooms_list.append(room)
+            print(f"Room [{room_name}] created by client: {nickname}")
         else:
-            senderindex = nicknames.index(message_from)
-            target_client = allclients[senderindex]
-            target_client.send(f'user [{message_to}] does not exist'.encode('utf-8'))
-    except:
-        print("Error occured!")
+            client.send("DISCONNECTED".encode('utf-8'))
+            client.close()
+            print(f"connection {str(address)} terminated")
+            return
+    
+    #save client data to server
+    nicknames.append(nickname)
+    rooms_list[roomindex].add_client(client)
+    all_clients.append(client)
+    
+    print(f'Nickname of the client is {nickname}, Room: {room_name}')
+    create_banner(client)
+    broadcast_to_clients_in_room(roomindex, f'{nickname} has joined the room: {room_name}!'.encode('utf-8'))
+    
+    client.send("CONNECTED".encode('utf-8'))
+    handle_client_actions(client, roomindex)
+        
+
+#creates a banner for the user to welcome and guide
+#uses sleep to help client handle incoming messages        
+def create_banner(client):
+    client.send('########################################'.encode('utf-8'))
+    time.sleep(0.01)
+    client.send('#          Welcome to ChatApp!!        #'.encode('utf-8'))
+    time.sleep(0.01)
+    client.send('#  Use @ to whisper and /exit to leave #'.encode('utf-8'))
+    time.sleep(0.01)
+    client.send('########################################'.encode('utf-8'))
+    time.sleep(0.1)
+
+
+def get_room_index(room_name):
+    for index, room in enumerate(rooms_list):
+        if room.name == room_name:
+            return index
+        
+    return NON_EXISTENT
+        
 
 #Handles the clients actions
-def handle(client,roomindex):
+def handle_client_actions(client,roomindex):
     while True:
         try:
-            msg = message = client.recv(1024)
-            if msg.decode('utf-8').startswith('AT'):
-                #used to send private messages
+            message = client.recv(BUFFER_SIZE)
+            
+            #Handle private messages
+            if message.decode('utf-8').startswith('AT'):
                 try:
-                    data = msg.decode('utf-8')[2:].split(' ',3)
+                    data = message.decode('utf-8')[2:].split(' ',3)
                     message_from = data[1]
                     target_client = data[2]
                     message_to_client = data[3]
                 except:
                     continue
-                private_message(target_client, message_from, message_to_client)
-            elif msg.decode('utf-8') == ('EXIT'):
-                #forces except to happen
-                index[-1]
+                private_message_client(target_client, message_from, message_to_client)
+            #Handle client manual disconnect
+            elif message.decode('utf-8') == ('EXIT'):
+                close_client_connection(client,roomindex)
+                break
             else:
-                #broadcasts message to others in same room
-                broadcast(roomindex,message)
+                broadcast_to_clients_in_room(roomindex,message)
         except:
-            #if the handling fails, the client connection is terminated, and client data removed
-            #index = rooms[roomindex].index(client)
-            rooms[roomindex].remove(client)
-            index = allclients.index(client)
-            allclients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            nicknames.remove(nickname)
-            print(f'{nickname} disconnected')
-            broadcast(roomindex, f'{nickname} has left the chat'.encode('utf-8'))
+            close_client_connection(client,roomindex)
             break
+        
+       
+def broadcast_to_clients_in_room(roomindex,message):
+    for client in rooms_list[roomindex].clients:
+        client.send(message)
+        
 
-#Recieves and processes the client connections
-def recieve():
-    while True:
-        client, address = server.accept()
-        print(f"connected with {str(address)}")
-        #recieve client nickname and room
-        client.send('ROOM'.encode('utf-8'))
-        room = client.recv(1024).decode('utf-8')
-        client.send('NICK'.encode('utf-8'))
-        nickname = client.recv(1024).decode('utf-8')
-        #check nickname and room availability and add client to corresponding room
-        roomindex = handle_room(room)
-        if nickname in nicknames:
-            client.send(f'Nickname [{nickname}] already taken'.encode('utf-8'))
-            time.sleep(2)
-            client.close()
-            print(f"connection {str(address)} terminated")
-            continue
-        elif(roomindex == -1):
-            client.send(f'Room [{room}] doesnt exist'.encode('utf-8'))
-            time.sleep(2)
-            client.close()
-            print(f"connection {str(address)} terminated")
-            continue
+def private_message_client(message_to,message_from, message_to_client):
+    try:
+        if message_to in nicknames:
+            targetindex = nicknames.index(message_to)
+            target_client = all_clients[targetindex]
+            target_client.send(f'FROM {message_from}: {message_to_client}'.encode('utf-8'))
         else:
-            nicknames.append(nickname)
-            rooms[roomindex].append(client)
-            allclients.append(client)
-            print(f'Nickname of the client is {nickname}, Room: {room}')
-            #creates a banner for the user to welcome and guide
-            #sleep to help client handle
-            client.send('########################################'.encode('utf-8'))
-            time.sleep(0.01)
-            client.send('#          Welcome to ChatApp!!        #'.encode('utf-8'))
-            time.sleep(0.01)
-            client.send('#  Use @ to whisper and /exit to leave #'.encode('utf-8'))
-            time.sleep(0.01)
-            client.send('########################################'.encode('utf-8'))
-            time.sleep(1)
-            broadcast(roomindex, f'{nickname} has joined the room: {room}!'.encode('utf-8'))
-
-            #threading so we can handle multiple clients at the same time
-            thread = threading.Thread(target=handle, args=(client,roomindex))
-            thread.start()
-
-#used to return desired index for room
-def handle_room(room):
-    if room == "Room1":
-        return 0
-    elif room == "Room2":
-        return 1
-    elif room == "Room3":
-        return 2
-    else:
-        return -1
-
-print("Server up and running!")
+            senderindex = nicknames.index(message_from)
+            target_client = all_clients[senderindex]
+            target_client.send(f'user [{message_to}] does not exist'.encode('utf-8'))
+    except:
+        print("Error occured!")      
+        
+        
+def close_client_connection(client,room_index):
+    #the client connection is terminated, and client data removed
+    rooms_list[room_index].remove_client(client)
+    nickname = nicknames[all_clients.index(client)]
+    nicknames.remove(nickname)
+    all_clients.remove(client)
+    client.close()
+    print(f'{nickname} disconnected')
+    broadcast_to_clients_in_room(room_index, f'{nickname} has left the chat'.encode('utf-8'))
+        
+     
+print(f'Server up and running on port: {port}')
 #main method to run the server
-recieve()
+search_client_connections()
 
 
